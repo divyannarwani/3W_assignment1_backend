96eb33379eacb2d32d36d6a9fd9b456c33619e71
divyannarwani <divyannarwani14@gmail.com>
2024-10-16 19:29:52 +0530

    new change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,52 +4,47 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-
-
 const createUser = asyncHandler ( async (req, res) => {
 
-    try {
-        const {name, socialMediaHandle} = req.body
-    
-        if (name.trim()===""){
-            throw new ApiError(400, "Name is required.")
-        }
-    
-        if (socialMediaHandle.trim()===""){
-            throw new ApiError(400, "Social Media Handle is required.")
-        }
-    
-        const imageFiles = req.files
-    
-        if (!imageFiles || imageFiles.length === 0){
-            throw new ApiError(400, "At least one image is required.")
-        }
-    
-        const uploadImages = await Promise.all(
-            imageFiles.map(async (file) => {
-                const uploadedFile = await uploadOnCloudinary(file.path)
-                return uploadedFile.url
-            })
-        )
-    
-        const user = await User.create({
-            name,
-            socialMediaHandle,
-            images: uploadImages
-        })
-    
-        const userCreated = await User.findById(user._id)
-    
-        if (!userCreated){
-            throw new ApiError(500, "Something went wrong at our end.")
-        }
-    
-        return res
-        .status(201)
-        .json(new ApiResponse(201, userCreated, "Data sent successfully."))
-    } catch (error) {
-        console.log("The problem is- ", error);
+    const {name, socialMediaHandle} = req.body
+
+    if (name.trim()===""){
+        throw new ApiError(400, "Name is required.")
     }
+
+    if (socialMediaHandle.trim()===""){
+        throw new ApiError(400, "Social Media Handle is required.")
+    }
+
+    const imageFiles = req.files
+
+    if (!imageFiles || imageFiles.length === 0){
+        throw new ApiError(400, "At least one image is required.")
+    }
+
+    const uploadImages = await Promise.all(
+        imageFiles.map(async (file) => {
+            const uploadedFile = await uploadOnCloudinary(file.path)
+            return uploadedFile.url
+        })
+    )
+
+    const user = await User.create({
+        name,
+        socialMediaHandle,
+        images: uploadImages
+    })
+
+    const userCreated = await User.findById(user._id)
+
+    if (!userCreated){
+        throw new ApiError(500, "Something went wrong at our end.")
+    }
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, userCreated, "Data sent successfully."))
+    
 })
 
 const getData = asyncHandler( async (req, res) => {
