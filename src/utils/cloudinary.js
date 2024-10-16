b765066f6d2207ab9c76f09c0e503a8839795112
divyannarwani <divyannarwani14@gmail.com>
2024-10-16 20:07:10 +0530

    unlink

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,11 +18,10 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         // file has been uploaded successfully
         // console.log("FILE IS UPLOADED ON CLOUDINARY ", response.url);
-        fs.unlinkSync(localFilePath)
         return response;  
 
     } catch (error) {
-        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed.
+         //remove the locally saved temporary file as the upload operation got failed.
         return null;
     }
 }
