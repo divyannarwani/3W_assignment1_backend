609d3b06cfff9c3d9b978095930458d338041391
divyannarwani <divyannarwani14@gmail.com>
2024-10-16 17:44:39 +0530

    some minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,44 +8,48 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const createUser = asyncHandler ( async (req, res) => {
 
-    const {name, socialMediaHandle} = req.body
-
-    if (name.trim()===""){
-        throw new ApiError(400, "Name is required.")
-    }
-
-    if (socialMediaHandle.trim()===""){
-        throw new ApiError(400, "Social Media Handle is required.")
-    }
-
-    const imageFiles = req.files
-
-    if (!imageFiles || imageFiles.length === 0){
-        throw new ApiError(400, "At least one image is required.")
-    }
-
-    const uploadImages = await Promise.all(
-        imageFiles.map(async (file) => {
-            const uploadedFile = await uploadOnCloudinary(file.path)
-            return uploadedFile.url
+    try {
+        const {name, socialMediaHandle} = req.body
+    
+        if (name.trim()===""){
+            throw new ApiError(400, "Name is required.")
+        }
+    
+        if (socialMediaHandle.trim()===""){
+            throw new ApiError(400, "Social Media Handle is required.")
+        }
+    
+        const imageFiles = req.files
+    
+        if (!imageFiles || imageFiles.length === 0){
+            throw new ApiError(400, "At least one image is required.")
+        }
+    
+        const uploadImages = await Promise.all(
+            imageFiles.map(async (file) => {
+                const uploadedFile = await uploadOnCloudinary(file.path)
+                return uploadedFile.url
+            })
+        )
+    
+        const user = await User.create({
+            name,
+            socialMediaHandle,
+            images: uploadImages
         })
-    )
-
-    const user = await User.create({
-        name,
-        socialMediaHandle,
-        images: uploadImages
-    })
-
-    const userCreated = await User.findById(user._id)
-
-    if (!userCreated){
-        throw new ApiError(500, "Something went wrong at our end.")
+    
+        const userCreated = await User.findById(user._id)
+    
+        if (!userCreated){
+            throw new ApiError(500, "Something went wrong at our end.")
+        }
+    
+        return res
+        .status(201)
+        .json(new ApiResponse(201, userCreated, "Data sent successfully."))
+    } catch (error) {
+        console.log("The problem is- ", error);
     }
-
-    return res
-    .status(201)
-    .json(new ApiResponse(200, userCreated, "Data sent successfully."))
 })
 
 const getData = asyncHandler( async (req, res) => {
